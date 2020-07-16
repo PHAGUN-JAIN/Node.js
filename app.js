127d8d14b87e250a0b06dd9ec2860c22dfe91378
PHAGUN JAIN <53911145+PHAGUN-JAIN@users.noreply.github.com>
2020-07-16 16:53:36 +0530

    async module

--- app.js
+++ app.js
@@ -4,4 +4,7 @@
   const files = fs.readdirSync('./');
   console.log(files);
 
-  
+  fs.readdir('./', function(err, files){
+        if (err) console.log()'Error', err;
+        else console.log('Result', files);
+ }); 
