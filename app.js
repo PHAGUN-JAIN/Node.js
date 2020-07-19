d105a5bdc056d61700b154bfca051fcc66085dff
PHAGUN JAIN <53911145+PHAGUN-JAIN@users.noreply.github.com>
2020-07-19 17:26:52 +0530

    test run

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 
-  const fs = require('fs');
+ // we need a library for the server set up http
+ 
+  const http = require('http')
+  const port = 3000  //the port we are going to use
+  
+  const server = http.createServer(function(req,res){
 
-//  const files = fs.readdirSync('./');
-  //console.log(files);
+           })
+//setting up server
 
-  fs.readdir('./', function(err, files){
-        if (err) console.log('Error', err);
-        else console.log('Result', files);
- }); 
+ server.listen(port, function(error){
+     if (error) {
+           console.log('Something went wrong', error)
+            } else {
+           console.log('Server is listening on port' + port)
+                }
+           })
