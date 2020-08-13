366f74170a47587df3e04bcb756dbae8e4340fef
PHAGUN JAIN <53911145+PHAGUN-JAIN@users.noreply.github.com>
2020-08-13 18:50:22 +0530

    calling all properties of other module

--- app3.js
+++ app3.js
@@ -1,3 +1,5 @@
  var stuff = require('./stuff');          //storing the properties of required module in a variable named stuff
 
  console.log(stuff.counter(['jain','java','c']));
+ console.log(stuff.adder(5,6));
+ console.log(stuff.adder(stuff.pi,5));
