aff4ac3d2b7c23ec9fc09081ee4b2409f5118a3a
PHAGUN JAIN <53911145+PHAGUN-JAIN@users.noreply.github.com>
2020-08-13 17:47:43 +0530

    in nodejs normal function in javascript works

---
+++ App1.js
@@ -0,0 +1,5 @@
+//normal function in javascript
+function sayHi(){
+  console.log("hi");
+}
+sayHi();
--- app.js
+++ app.js
@@ -1,19 +1,5 @@
-
- // we need a library for the server set up http
- 
-  const http = require('http')
-  const port = 3000  //the port we are going to use
-  
-  const server = http.createServer(function(req,res){
-         res.write('Hello Node')
-         res.end()
-           })
-//setting up server
-
- server.listen(port, function(error){
-     if (error) {
-           console.log('Something went wrong', error)
-            } else {
-           console.log('Server is listening on port' + port)
-                }
-           })
+var time = 0;
+var timer = setInterval(function(){
+  time +=2;
+  console.log(time+'seconds have passed');
+},2000);
