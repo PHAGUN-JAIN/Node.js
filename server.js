7d126a95c0cef7b451b2be99eee280a9683f3ae0
PHAGUN JAIN <53911145+PHAGUN-JAIN@users.noreply.github.com>
2020-08-15 13:53:40 +0530

    testing server

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-
+/*
 var http = require('http');
 var server = http.createServer(function(req, res){
   console.log('request was made :'+ req.url);
@@ -8,3 +8,4 @@ var server = http.createServer(function(req, res){
 
 server.listen(3000, '127.0.0.1');
 console.log('now listening to port 3000');
+*/
