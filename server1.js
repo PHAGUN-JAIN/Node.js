5aa131831ea9128fd64d37854fca1e8f107ec1c6
PHAGUN JAIN <53911145+PHAGUN-JAIN@users.noreply.github.com>
2020-08-15 14:20:16 +0530

    serving webpages:))

--- server1.js
+++ server1.js
@@ -3,8 +3,8 @@ var fs = require('fs');
 
 var server= http.createServer(function(req,res){
   console.log('request was made:' + req.url);
-  res.writeHead(200,{'Content-Type':'Text/plain'});
-  var myReadStream = fs.createReadStream(__dirname + '/Streams.txt','utf-8');
+  res.writeHead(200,{'Content-Type':'Text/html'});
+  var myReadStream = fs.createReadStream(__dirname + '/index.html','utf-8');
   myReadStream.pipe(res);   //pipe method woks only for readable stream because directing is possible from sourec and res here is a writable stream
 });
 
