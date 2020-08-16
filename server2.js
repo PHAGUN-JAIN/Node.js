32e025d86a5063616ff8d8b38d63ca379d311eb2
PHAGUN JAIN <53911145+PHAGUN-JAIN@users.noreply.github.com>
2020-08-16 14:33:40 +0530

    sending json data

--- server2.js
+++ server2.js
@@ -9,6 +9,10 @@ var server = http.createServer(function(req,res){
   }else if (req.url === '/contact') {
       res.writeHead(200,{'Content-Type':'Text/html'});
       fs.createReadStream(__dirname + '/Contact.html','utf-8').pipe(res);
+  }else if (req.url==='/api/ninjas') {
+    var ninjas = [{name:'james',age:22},{name:'ryu',age:32}]
+    res.writeHead(200,{'Content-Type':'application/json'});
+    res.end(JSON.stringify(ninjas));
   }
  });
 
