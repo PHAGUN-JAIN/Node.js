82fc513c7f5ecbae56e492e88b2506fd6cdb1a5f
PHAGUN JAIN <53911145+PHAGUN-JAIN@users.noreply.github.com>
2020-08-16 14:24:40 +0530

    basic routing contd...

--- server2.js
+++ server2.js
@@ -6,8 +6,11 @@ var server = http.createServer(function(req,res){
   if(req.url ==='/home' || req.url ==='/'){
     res.writeHead(200,{'Content-Type':'Text/html'});
     fs.createReadStream(__dirname + '/index.html','utf-8').pipe(res);
+  }else if (req.url === '/contact') {
+      res.writeHead(200,{'Content-Type':'Text/html'});
+      fs.createReadStream(__dirname + '/Contact.html','utf-8').pipe(res);
   }
-});
+ });
 
   server.listen(3000,'127.0.0.1');
   console.log('listening tot port 3000');
