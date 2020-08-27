24dc7e8e2bc6349c12f502e0e8b791ced4824b71
PHAGUN JAIN <53911145+PHAGUN-JAIN@users.noreply.github.com>
2020-08-27 19:32:16 +0530

    added request handlerds to the route

--- Rest-Shop/api/routes/orders.js
+++ Rest-Shop/api/routes/orders.js
@@ -1,2 +1,30 @@
 const express=require('express');
 const router=express.Router();
+
+router.get('/', (req, res, next)=>{
+  res.status(200).json({
+    message:"Orders were fetched"
+  });
+});
+
+router.post('/', (req, res, next)=>{
+  res.status(201).json({
+    message:"Orders were fetched"
+  });
+});
+
+router.get('/:orderId', (req, res, next)=>{
+  res.status(200).json({
+    message:"Order Details!"
+    
+  });
+});
+
+router.dellete('/:orderId', (req, res, next)=>{
+  res.status(200).json({
+    message:"Order Deleted!"
+
+  });
+});
+
+module.exports=router;
--- Rest-Shop/app.js
+++ Rest-Shop/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders')
 //this is the middleware from where all the requests are funneled through
 /*
 app.use((req, res, next)=>{
@@ -11,4 +12,5 @@ app.use((req, res, next)=>{
 */
 //now the work of this middleware will be to trannsfer the request to productRoutes
 app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
 module.exports=app;
